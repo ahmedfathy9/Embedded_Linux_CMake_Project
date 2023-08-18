71802656e5bc25b3d6ae8e1942d8784756029080
Ahmed Fathy <95315791+ahmedfathy9@users.noreply.github.com>
2023-08-18 23:24:39 +0300

    Add files via upload

---
+++ build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ahmedfathy98/ELinuxSprints/tasks/task4_module2/project_dir/src/main.cpp" "CMakeFiles/program.dir/src/main.cpp.o" "gcc" "CMakeFiles/program.dir/src/main.cpp.o.d"
+  "/home/ahmedfathy98/ELinuxSprints/tasks/task4_module2/project_dir/src/print.cpp" "CMakeFiles/program.dir/src/print.cpp.o" "gcc" "CMakeFiles/program.dir/src/print.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ahmedfathy98/ELinuxSprints/tasks/task4_module2/project_dir/build/CMakeFiles/write_in_file.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
